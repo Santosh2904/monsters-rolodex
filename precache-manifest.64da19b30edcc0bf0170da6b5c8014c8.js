eefa7ad277f1f357b77adc2377a124f67bde059e
Santosh Majji <santoshmajji29@gmail.com>
2020-08-04 15:40:02 +0530

    Updates

---
+++ precache-manifest.64da19b30edcc0bf0170da6b5c8014c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b64bef8785d7ca7970a91401ff928c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8ee4319d827ca4d6d472",
+    "url": "/monsters-rolodex/static/css/main.e0151e5a.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee4319d827ca4d6d472",
+    "url": "/monsters-rolodex/static/js/main.8ecb6c49.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
